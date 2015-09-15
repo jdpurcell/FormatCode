b37b4848d97c13c4b2a8e0af0e378257f34e3b89
J.D. Purcell <jdpurcell@gmail.com>
2015-09-15 10:52:45 -0400

    Initial commit.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace FormatCode {
+	partial class frmMain {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.SuspendLayout();
+			// 
+			// frmMain
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(194, 175);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Code Formatter";
+			this.TopMost = true;
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+	}
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------------------------
+// Copyright 2011 J.D. Purcell
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ---------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FormatCode {
+	public partial class frmMain : Form {
+		public frmMain() {
+			InitializeComponent();
+		}
+
+		private void frmMain_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+				e.Effect = DragDropEffects.Copy;
+			}
+		}
+
+		private void frmMain_DragDrop(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+				string[] ignoreNames = new string[] {  };
+				string[] ignoreSuffixes = new string[] {  };
+				string[] ignoreDirectories = new string[] {  };
+				string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+				List<IEnumerable<string>> pathEnumList = new List<IEnumerable<string>>();
+				foreach (string path in paths) {
+					if (File.Exists(path)) {
+						pathEnumList.Add(new[] { path });
+					}
+					else if (Directory.Exists(path)) {
+						pathEnumList.Add(Directory.EnumerateFiles(path, "*.cs", SearchOption.AllDirectories));
+					}
+				}
+				foreach (string path in pathEnumList.SelectMany(pe => pe)) {
+					if (!Path.GetExtension(path).Equals(".cs", StringComparison.OrdinalIgnoreCase)) continue;
+					if (ignoreNames.Any(n => Path.GetFileName(path).Equals(n, StringComparison.OrdinalIgnoreCase))) continue;
+					if (ignoreSuffixes.Any(s => path.EndsWith(s, StringComparison.OrdinalIgnoreCase))) continue;
+					if (ignoreDirectories.Any(d => path.IndexOf(@"\" + d + @"\", StringComparison.OrdinalIgnoreCase) != -1)) continue;
+					CodeFormatter.Format(path);
+				}
+				Activate();
+				MessageBox.Show(this, "Done!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+	}
+}
